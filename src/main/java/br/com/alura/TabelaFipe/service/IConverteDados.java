5cf13075aec9becf05205484add249c69ae28ad3
fabionc <fabionc@unicamp.br>
2025-01-06 10:01:59 -0300

    Create class veiculo for interpret format Veiculo and create rules bussines

--- src/main/java/br/com/alura/TabelaFipe/service/IConverteDados.java
+++ src/main/java/br/com/alura/TabelaFipe/service/IConverteDados.java
@@ -1,5 +1,9 @@
 package br.com.alura.TabelaFipe.service;
 
+import java.util.List;
+
 public interface IConverteDados {
     <T> T obterDados(String Json, Class<T> classe);
+
+    <T> List<T> obeterLista(String json, Class<T> classe);
 }
